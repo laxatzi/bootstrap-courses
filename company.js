f9bd758250a032043dff506106558f29d3ad487a
Lambros Chatzinikolaou <39900010+laxatzi@users.noreply.github.com>
2018-09-24 12:48:23 +0300

    edit google maps zoom

--- company.js
+++ company.js
@@ -5,7 +5,7 @@ function initMap() {
   var uluru = { lat: 40.736851, lng: 22.920227};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('myGoogleMap'), {zoom: 4, center: uluru});
+      document.getElementById('myGoogleMap'), {zoom: 10, center: uluru});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
